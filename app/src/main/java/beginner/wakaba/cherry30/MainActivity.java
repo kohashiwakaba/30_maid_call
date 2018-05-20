bee35ef07f58d7f5ee95484b5e31616a12950fbc
코하시와카바 <37092106+kohashiwakaba@users.noreply.github.com>
2018-05-20 16:10:08 +0900

    Initial commit

---
+++ app/src/main/java/beginner/wakaba/cherry30/MainActivity.java
@@ -0,0 +1,89 @@
+package beginner.wakaba.cherry30;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] maids = {"체리","비스티","유리","하나"};
+    Spinner sm;
+    EditText em,eq,en;
+    Button bc;
+    Button bt;
+    View.OnClickListener cl;
+    View.OnClickListener cc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sm = findViewById(R.id.sel_maid);
+
+        em = findViewById(R.id.t_menu);
+        eq = findViewById(R.id.t_qty);
+        en = findViewById(R.id.t_new);
+
+        bc = findViewById(R.id.call);
+        bt = findViewById(R.id.cont);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,maids);
+        sm.setAdapter(adapter);
+
+        cl = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent f = new Intent();
+                f.setAction("cherry.call");
+                f.setPackage("beginner.wakaba.cherry31");
+                f.putExtra("maid",sm.getSelectedItem().toString());
+                f.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+
+                String fm = em.getText().toString();
+                if (fm!=""){
+                    f.putExtra("menu",fm);
+                }else{
+                    f.putExtra("menu","카푸치노");
+                }
+
+                String fq = eq.getText().toString();
+                if (fq!=""){
+                    f.putExtra("quantity",fq);
+                }else{
+                    f.putExtra("quantity","1");
+                }
+
+                sendBroadcast(f);
+            }
+        };
+
+        cc = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newmaid = en.getText().toString();
+                if (newmaid!=""){
+                    Intent c = new Intent();
+                    c.setAction("cherry.contract");
+                    c.setPackage("beginner.wakaba.cherry31");
+                    c.putExtra("maid_cont",newmaid);
+                    c.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+                    sendBroadcast(c);
+                }else{
+                    Toast.makeText(getApplicationContext(), "신청하실 메이드 이름을 입력해 주십시오.",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        };
+
+        bc.setOnClickListener(cl);
+        bt.setOnClickListener(cc);
+
+    }
+}
